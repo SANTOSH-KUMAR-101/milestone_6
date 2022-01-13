e7598854d3d200b2d6d627551211de6d0513c092
SANTOSH KUMAR <santosk.kumar.101@gmail.com>
2022-01-13 16:28:34 +0530

    Update and rename JSON_conversions.js to Practise_1_JSON_conversions.js

---
+++ Practise_1_JSON_conversions.js
@@ -0,0 +1,18 @@
+//  How to Convert object from JSON Object and vice versa
+
+var object1 = {
+    name: "santosh",
+    code: "JSON"
+}
+
+console.log(object1);
+console.log(object1.name);
+
+var JSON1 = JSON.stringify(object1);
+
+console.log(JSON1);
+console.log(JSON1.name);
+var JSON_to_Obj = JSON.parse(JSON1);
+
+console.log(JSON_to_Obj);
+console.log(JSON_to_Obj.name);
